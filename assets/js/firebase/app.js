9ffe808ed2933187b876f402a15e62f77ae14bfb
Michel Maia <programador.michel.maia@gmail.com>
2022-09-06 10:12:52 -0300

    commit

---
+++ assets/js/firebase/app.js
@@ -0,0 +1,18 @@
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.4/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyAkkF7LKyDvztDaoo9CMgZVv7GY60-1HRw",
+    authDomain: "projeto-frontmundo-inver-42eb4.firebaseapp.com",
+    projectId: "projeto-frontmundo-inver-42eb4",
+    storageBucket: "projeto-frontmundo-inver-42eb4.appspot.com",
+    messagingSenderId: "1056531591201",
+    appId: "1:1056531591201:web:9ebb4b28702198cf877360"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
--- assets/js/main.js
+++ assets/js/main.js
@@ -19,3 +19,22 @@ audio.play();
 audio.volume = 0.2;
 
 }
+
+const txtName = document.getElementById('txtName')
+const txtEmail = document.getElementById('txtEmail')
+const txtLevel = document.getElementById('txtLevel')
+const txtCharacter = document.getElementById('txtCharacter')
+
+const btnSubscribe = document.getElementById('btnSubscribe')
+
+btnSubscribe.addEventListener('click', () => {
+    const subscription = {
+        name: txtName.value,
+        email: txtEmail.value,
+        level: txtLevel.value,
+        character: txtCharacter.value
+    }
+
+    console.log(subscription)
+})
+
